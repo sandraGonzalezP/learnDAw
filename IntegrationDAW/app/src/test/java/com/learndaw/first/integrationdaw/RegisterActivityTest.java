ad6f13daff60a7668cee0300d4fcbd521eab30eb
sandraGonzalezP <32445461+sandraGonzalezP@users.noreply.github.com>
2018-08-22 11:42:16 -0500

    Add files via upload

---
+++ IntegrationDAW/app/src/test/java/com/learndaw/first/integrationdaw/RegisterActivityTest.java
@@ -0,0 +1,14 @@
+package com.learndaw.first.integrationdaw;
+
+import org.junit.Test;
+
+public class RegisterActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+}
